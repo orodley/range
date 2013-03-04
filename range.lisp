6470c30dd9a65509410174e2107b3469dbcb1c9a
Owen Rodley <Strigoides@gmail.com>
2013-03-04 20:02:45 +1300

    More robust error checking

--- range.lisp
+++ range.lisp
@@ -6,8 +6,7 @@
   "Given the text within a range expression, e.g.:
    `1..100' or `32,35..47' or `10,9..0', return the corresponding list"
   (declare (string range-string))
-  ;; Check that the range is well-formed
-  (unless (cl-ppcre:scan "\\d+(,\\d+)?\\.\\.\\d+" range-string)
+  (unless (cl-ppcre:scan "^\\d+(,\\d+)?\\.\\.\\d+$" range-string)
     (error "Badly formed range #[~A]" range-string))
   (let* ((sections (cl-ppcre:all-matches-as-strings "\\d+" range-string))
          (start (read-from-string (first sections)))
