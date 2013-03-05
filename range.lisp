007a3011bcd9f33c4eef81de0518de6f7adfba22
Owen Rodley <Strigoides@gmail.com>
2013-03-05 14:57:25 +1300

    Allow negative numbers and floats

--- range.lisp
+++ range.lisp
@@ -6,9 +6,23 @@
   "Given the text within a range expression, e.g.:
    `1..100' or `32,35..47' or `10,9..0', return the corresponding list"
   (declare (string range-string))
-  (unless (cl-ppcre:scan "^ *\\d+( *, *\\d+)? *\\.\\. *\\d+$" range-string)
+  (declare (optimize (debug 3)))
+  (unless (cl-ppcre:scan 
+            "(?x) # Enable optional whitespace + comments
+             ^[ ]*
+             # Start
+             -?\\d+(\\.\\d+)?
+             # Optional step
+             ([ ]*,[ ]*-?\\d+(\\.\\d+)?)?
+             # ..
+             [ ]*\\.\\.
+             # End
+             [ ]*-?\\d+(\\.\\d+)?
+             [ ]*$" range-string)
     (error "Badly formed range #[~A]" range-string))
-  (let* ((sections (cl-ppcre:all-matches-as-strings "\\d+" range-string))
+  (let* ((sections (cl-ppcre:all-matches-as-strings
+                     "-?\\d+(\\.?\\d+)?"
+                     range-string))
          (start (read-from-string (first sections)))
          (step (if (= (length sections) 3)
                  (- (read-from-string (second sections))
